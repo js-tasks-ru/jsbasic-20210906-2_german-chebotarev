4a62645aca465c7b659071e8f978081c0084e5f3
sv-yaz-ist <german.chebotarev@gmail.com>
2021-09-13 17:40:20 +0300

    c 1й по 4ю задачи 1го модуля

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,16 @@
 function factorial(n) {
-  // ваш код...
+  if (n == 0 || n == 1) {
+    return 1;
+  }
+  let result = n;
+
+  do {
+    result = result * (n - 1);
+    n = n - 1;
+  }
+
+  while (n > 1);
+  return result;
 }
+
+console.log(factorial(5));
--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,7 +1,7 @@
 let maxlength = 20;
 function truncate(str, maxlength) {
   if (str.length > maxlength) {
-    return str.slice(0, maxlength-1) + '\u2026';
+    return str.slice(0, maxlength -1) + '\u2026';
   }
   return str;
 }
